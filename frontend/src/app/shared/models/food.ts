2084af4f34e33b9788d4e6685ad89ad0b00edfe5
Saksham <sakshamsharma8982@gmail.com>
2023-12-21 11:39:14 +0530

    Home Component Created

---
+++ frontend/src/app/shared/models/food.ts
@@ -0,0 +1,11 @@
+export class Food{
+    id!:string;
+    name!:string;
+    price!:number;
+    tags?:string[];
+    favorite!:boolean;
+    stars!:number;
+    origins!:string[];
+    imageUrl!:string;
+    cookTime!:string;
+}
