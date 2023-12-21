25eee34065314312f30a9c5c56e4d20c52ed6b91
Saksham <sakshamsharma8982@gmail.com>
2023-12-21 16:40:06 +0530

    Created Search Component

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,13 @@ import { FoodService } from '../../../services/food.service';
 export class HomeComponent {
   foods:Food[] = [];
 
-  constructor(private foodService:FoodService){
-    this.foods = foodService.getAll();
+  constructor(private foodService:FoodService, activatedRoute:ActivatedRoute){
+    activatedRoute.params.subscribe((params)=>{
+      if(params.searchTerm)
+      this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else
+      this.foods = foodService.getAll();
+    })
   }
 }
 
